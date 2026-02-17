63b0506348cf5f53f3d12c9baae2f8d6e49d78b0
AssalbekulyAlmaz <a_assalbekuly@kbtu.kz>
2026-02-17 23:53:55 +0500

    Add files via upload

---
+++ lab-3/task1/chapter2.js
@@ -0,0 +1,20 @@
+const sayHelloAndWorld = () => {
+  alert('Hello');
+  alert('World');
+};
+
+const sumThreeNumbers = () => {
+  const result = 3
+    + 1
+    + 2;
+
+  return result;
+};
+
+const commentedExample = () => {
+  
+  const message = 'Code structure demo';
+  return message;
+};
+
+
